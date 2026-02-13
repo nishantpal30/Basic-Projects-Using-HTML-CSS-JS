e58b88873768b554d8ef8a0a9c14b76ab258c2ad
Nishant Pal <nishantpal708853@gmail.com>
2026-02-13 23:07:21 +0530

    card ban gyaa

---
+++ Productivity DashBoard/app.js
@@ -0,0 +1,14 @@
+let cardbtn = document.querySelectorAll(".cardbtn");
+let contentbox = document.querySelectorAll(".cont-elems");
+
+// cardbtn.forEach((value)=>{
+// value.addEventListener("click" , (e)=>{
+// console.log(e.textcontent)
+// })
+// })
+
+
+// cardbtn.addEventListner("click",()=>{
+//     console.log(hii)
+// })
+console.log(cardbtn.innerHTML)
