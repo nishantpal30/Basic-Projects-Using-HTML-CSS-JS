f83ce0569208285886b4e42f44d81d0ccbb3e78e
Nishant Pal <nishantpal708853@gmail.com>
2026-02-17 23:42:56 +0530

    some frontend ready(card)

---
+++ Producivity Dashboard/app.js
@@ -0,0 +1,8 @@
+let card1 =document.querySelector(".main-card1");
+let todopage =document.querySelector(".todopage");
+
+card1.addEventListener("click",()=>{
+//     todopage.classList.toggle(".show");
+    todopage.classList.toggle("show")
+})
+
