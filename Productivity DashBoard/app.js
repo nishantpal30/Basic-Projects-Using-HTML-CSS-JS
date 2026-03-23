8f30a01b54979c7c4f07fe3cd7121723d2818dcd
Nishant Pal <nishantpal708853@gmail.com>
2026-03-23 23:09:44 +0530

    local storage added

--- Productivity DashBoard/app.js
+++ Productivity DashBoard/app.js
@@ -4,72 +4,69 @@ let allpagesbackbtn = document.querySelectorAll(".main-pages .back");
 // todopage
 let todoform = document.querySelector(".todo-form");
 let todoinput = document.querySelector(".todo-area .addtask form input");
-let todotext = document.querySelector(".todo-area .addtask form textarea")
-let dlttask = document.querySelector(".todo-area .deletetask")
-let tsklist = document.querySelector(".deletetask .tasklists");
+let todotext = document.querySelector(".todo-area .addtask form textarea");
+let dlttask = document.querySelector(".todo-area .deletetask");
+// let btntask = document.querySelector(".todo-area .deletetask .tasklists button");
 
-allcards.forEach(function(elem){
-    elem.addEventListener("click",function(){
-allpages[elem.id].style.display="block";
-    })
-})
+allcards.forEach(function (elem) {
+  elem.addEventListener("click", function () {
+    allpages[elem.id].style.display = "block";
+  });
+});
 
-allpagesbackbtn.forEach(function(back){
-    
-    back.addEventListener("click" , ()=>{
-        allpages[back.id].style.display="none";
-    })
-})
+allpagesbackbtn.forEach(function (back) {
+  back.addEventListener("click", () => {
+    allpages[back.id].style.display = "none";
+  });
+});
 
 // todopage
 
-let currenttask=[];
-todoform.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    currenttask.push(
-    {
-        task:todoinput.value ,
-        detail: todotext.value
-    }
-    
-)
- rendertask();
-// console.log(currenttask)
-todoform.reset();
-})
+var currenttask = JSON.parse(localStorage.getItem("currenttask")) || [];
 
-function rendertask(){
-    dlttask.innerHTML = "";
-currenttask.forEach(function(task)
-    {
+window.addEventListener("DOMContentLoaded", () => {
+  rendertask();
+});
+
+todoform.addEventListener("submit", (e) => {
+  e.preventDefault();
+  currenttask.push({
+    task: todoinput.value,
+    detail: todotext.value,
+  });
+
+  localStorage.setItem("currenttask", JSON.stringify(currenttask));
+  rendertask();
+
+  todoform.reset();
+});
+
+function rendertask() {
+  dlttask.innerHTML = "";
+  currenttask.forEach(function (task) {
     const div = document.createElement("div");
     div.classList.add("tasklists");
-    div.innerHTML=`
+    div.innerHTML = `
+    <details>
+    <summary>
     <h5>${task.task}</h5>
+    </summary>
+    <p>${task.detail}</p></details>
+   
     <button>Mark's as Complete</button>
-    `;
+    `
+   
+    ;
 
     dlttask.appendChild(div);
-    
-});
+  });
 }
 
-tsklist.addEventListener("click",()=>{
-    
-    renderdetails();
-})
-function renderdetails(){
-    // dlttask.innerHTML = "";
-currenttask.forEach(function(task)
-    {
-    const div = document.createElement("div");
-    div.classList.add("taskdetails");
-    div.innerHTML=`
-    <p>${task.detail}</p>
-    `;
+document.addEventListener("click", (e) => {
+  const btn = e.target.closest(".todo-area .deletetask .tasklists button");
 
-    dlttask.appendChild(div);
-    
-});
+  if (btn) {
+    console.log(task.task);
 }
-
+}
+);
