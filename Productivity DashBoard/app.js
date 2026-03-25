b032ffd5b0109d9bde98dd3c3daa97de6b273a62
Nishant Pal <nishantpal708853@gmail.com>
2026-03-25 23:05:25 +0530

    weekly section ready

--- Productivity DashBoard/app.js
+++ Productivity DashBoard/app.js
@@ -1,12 +1,12 @@
 let allcards = document.querySelectorAll(".main-ele");
 let allpages = document.querySelectorAll(".main-pages");
 let allpagesbackbtn = document.querySelectorAll(".main-pages .back");
+let thoughts = document.querySelector(".thought h1");
 // todopage
 let todoform = document.querySelector(".todo-form");
 let todoinput = document.querySelector(".todo-area .addtask form input");
 let todotext = document.querySelector(".todo-area .addtask form textarea");
 let dlttask = document.querySelector(".todo-area .deletetask");
-
 
 allcards.forEach(function (elem) {
   elem.addEventListener("click", function () {
@@ -43,7 +43,7 @@ todoform.addEventListener("submit", (e) => {
 
 function rendertask() {
   dlttask.innerHTML = "";
-  currenttask.forEach(function (task , index) {
+  currenttask.forEach(function (task, index) {
     const div = document.createElement("div");
     div.classList.add("tasklists");
     div.innerHTML = `
@@ -54,9 +54,7 @@ function rendertask() {
     <p>${task.detail}</p></details>
    
     <button data-index="${index}">Mark's as Complete</button>
-    `
-   
-    ;
+    `;
 
     dlttask.appendChild(div);
   });
@@ -66,15 +64,53 @@ document.addEventListener("click", (e) => {
   const btn = e.target.closest(".todo-area .deletetask .tasklists button");
 
   if (btn) {
- let clickedIndex = Number(btn.dataset.index);
-  currenttask.splice(clickedIndex, 1);
+    let clickedIndex = Number(btn.dataset.index);
+    currenttask.splice(clickedIndex, 1);
 
     // update localStorage
     localStorage.setItem("currenttask", JSON.stringify(currenttask));
 
     // re-render UI
     rendertask();
-}
-}
-);
+  }
+});
+fetch("https://dummyjson.com/quotes/random")
+  .then((res) => res.json())
 
+  .then((data) => {
+    thoughts.textContent = data.quote;
+  });
+let weekform = document.querySelector(".weekly-table");
+var weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+function weekcard() {
+  weekdays.forEach((name) => {
+    let dayname = document.createElement("div");
+    dayname.classList.add("week-days");
+    let weekname = document.createElement("div");
+    weekname.classList.add("week-name");
+    let heading = document.createElement("h2");
+    heading.innerText = name;
+    weekname.appendChild(heading);
+
+    let textfiels = document.createElement("div");
+    textfiels.innerHTML = `
+    <textarea name="" id="" placeholder="Enter weekly goal...." rows="2" cols="19"></textarea>
+  
+    `;
+
+   dayname.appendChild(weekname);
+   dayname.appendChild(textfiels);
+   weekform.appendChild(dayname)
+  });
+}
+
+weekcard();
